b71010464e653fbffb03ab30634c2933cf313317
xentixar <xentixar@gmail.com>
2024-04-30 16:57:36 +0545

    [FIX]:add commands to my laravel project installer script

--- .config/my_scripts/create_laravel_project.sh
+++ .config/my_scripts/create_laravel_project.sh
@@ -1,3 +1,9 @@
 composer create-project laravel/laravel $1;
+cd $1;
 composer require barryvdh/laravel-debugbar --dev;
+sudo apt install php-pear php-dev;
+pecl install swoole;
+composer require laravel/octane;
+php artisan octane:install;
+php artisan octane:start --port=7777;
 
